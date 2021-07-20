880d1dd956fb553b0dd660ac51fddcb6781ae33d
BOB-THE-PENGUIN <86697455+BOB-THE-PENGUIN@users.noreply.github.com>
2021-07-20 13:30:32 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changecolor () {
+    document.getElementById("nameheading").style.color="yellow";
+}
+function changepara () {
+    document.getElementById("firstpara").style.color="red";
+   
+}
